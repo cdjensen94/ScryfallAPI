909dbb63079c865fad0d091341d5ca9425377885
iAmLam094 <lamineabdi994@gmail.com>
2024-03-07 21:43:34 +0100

    Refactor on saving user's favorites

--- ScryfallAPI/obj/Debug/net8.0/ScryfallAPI.AssemblyInfo.cs
+++ ScryfallAPI/obj/Debug/net8.0/ScryfallAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScryfallAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dcd17a733f9ab206f083eeb455fde140fff7989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bb6b4ab40946c0f684a55717a1e2c37dda2d670")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScryfallAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScryfallAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
